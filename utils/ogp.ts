83efa357821c14ba88ed487837654113ee7dfd90
Asazu Taiga <taiga.asazu@icloud.com>
2021-08-13 00:55:56 +0900

    change font of ogp

--- utils/ogp.ts
+++ utils/ogp.ts
@@ -1,4 +1,4 @@
 export const generateOgpUrl = (title: string) => {
   const encoded = encodeURI(title)
-  return `https://res.cloudinary.com/dtouqmixu/image/upload/s--KghSoe3v--/c_fit,g_north_west,l_text:Sawarabi%20Gothic_90:${encoded},w_1120,x_50,y_50/v1628769914/ogp_base_riirui.png`
+  return `https://res.cloudinary.com/dtouqmixu/image/upload/s--KghSoe3v--/c_fit,g_north_west,l_text:NotoSansJP.otf_90:${encoded},w_1120,x_50,y_50/v1628769914/ogp_base_riirui.png`
 }
