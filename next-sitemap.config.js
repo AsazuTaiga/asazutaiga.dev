e8f26b6cdd7146d2709096082581912274fcec6c
AsazuTaiga <taiga.asazu@icloud.com>
2023-01-08 19:08:22 +0900

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_VERCEL_URL || 'https://asazutaiga.dev',
+  siteUrl: 'https://asazutaiga.dev',
   generateRobotsTxt: true,
 }
--- utils/ogp.ts
+++ utils/ogp.ts
@@ -1,5 +1,5 @@
 export const generateOgpUrl = (title: string, emoji: string) => {
   const encoded = encodeURI(title)
-  const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL || 'https://asazutaiga.dev'
-  return `${baseUrl}/api/og?title=${encoded}&emoji=${emoji}`
+  const baseUrl = 'asazutaiga.dev'
+  return `https://${baseUrl}/api/og?title=${encoded}&emoji=${emoji}`
 }
